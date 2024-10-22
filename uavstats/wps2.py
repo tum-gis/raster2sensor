dfaab2a7e190e42901500b1cb53cb463744cffcb
Joseph Mureithi <joemureithi@live.com>
2024-10-22 18:34:34 +0200

    test python wps wrappers

--- uavstats/wps2.py
+++ uavstats/wps2.py
@@ -3,6 +3,7 @@ TEST: OWSLib WPS
 https://ouranosinc.github.io/pavics-sdi/tutorials/wps_with_python.html
 '''
 
+from collections import OrderedDict
 import os
 import sys
 import requests
@@ -25,8 +26,8 @@ console = Console()
 @dataclass
 class WPS:
     geoserver_url: str = config.GEOSERVER_URL
-    geoserver_user = config.GEOSERVER_USER
-    geoserver_password = config.GEOSERVER_PASSWORD
+    geoserver_user: str = config.GEOSERVER_USER
+    geoserver_password: str = config.GEOSERVER_PASSWORD
 
     def __post_init__(self):
         if not self.geoserver_url:
@@ -35,38 +36,38 @@ class WPS:
             geoserver_url=self.geoserver_url)
         self.wps_get_capabilities_url = config.WPS_GET_CAPABILITIES_URL.substitute(
             geoserver_url=self.geoserver_url)
+        self.headers = {'Content-Type': 'application/xml'}
+        self._wps = WebProcessingService(self.wps_url, version='1.0.0', username=self.geoserver_user,
+                                         password=self.geoserver_password, headers=self.headers)
 
     def get_capabilities(self) -> str:
         '''Get WPS Capabilities'''
-        get_capabilities = requests.get(self.wps_get_capabilities_url)
-        return get_capabilities.text
+        # get_capabilities = requests.get(self.wps_get_capabilities_url)
+        # return get_capabilities.text
+        return self._wps.getcapabilities(xml=None)
 
-    # def describe_wps_process(self, identifier: str) -> str:
-    #     '''Describe a WPS process
-    #         Args: identifier (str): WPS Process Identifier
-    #         Returns: response (str): WPS Process Description
-    #     '''
-    #     describe_process_url = f"{self.wps_url}&request=DescribeProcess&identifier={
-    #         identifier}"
-    #     print(f'[green]Describing WPS Process: {identifier}')
-    #     response = requests.get(describe_process_url)
-    #     return response.text
+    def get_processes(self) -> list:
+        '''Get WPS Processes'''
+        return self._wps.processes
 
-    # @timeit
-    # def execute_wps_process(self, payload: str, file_path: str = None):
-    #     headers = {'Content-Type': 'application/xml'}
-    #     response = requests.post(config.WPS_URL, data=payload, headers=headers, auth=(
-    #         config.GEOSERVER_USER, config.GEOSERVER_PASSWORD))
-    #     # Handle the response
-    #     if response.status_code == 200:
-    #         print("Request successful")
-    #         print(response.text)  # or process the JSON data as needed
-    #         if file_path:
-    #             with open(file_path, 'w') as file:
-    #                 json.dump(response.json(), file)
-    #     else:
-    #         print(f"Request failed with status code {response.status_code}")
-    #         print(response.text)
+    def describe_wps_process(self, identifier: str) -> str:
+        '''Describe a WPS process
+            Args: identifier (str): WPS Process Identifier
+            Returns: response (str): WPS Process Description
+        '''
+
+        process = self._wps.describeprocess(identifier)
+        return process
+
+    @timeit
+    def execute_wps_process(self, pid: str, inputs: list, outputs: list = None, mode: str = 'ASYNC', request: str = None):
+        try:
+            response = self._wps.execute(
+                pid, inputs=inputs, output=outputs, mode=mode, request=request)
+            return response
+        except Exception as e:
+            print(f'[red]Error: {e}')
+            return None
 
     # @staticmethod
     # def encode_geotiff_to_base64(file_path):
@@ -77,19 +78,14 @@ class WPS:
 
 if __name__ == '__main__':
     clear()
-    print(config.WPS_URL.substitute(
-        geoserver_url=config.GEOSERVER_URL))
-    # wps = WebProcessingService(config.WPS_URL.substitute(
-    #     geoserver_url=config.GEOSERVER_URL), skip_caps=True)
-    wps = WebProcessingService(
-        'http://localhost:8085/geoserver/ows', username='admin', password='geoserver')
-    # pprint(vars(wps))
+    wps = WPS(config.GEOSERVER_URL)
+    wps.get_capabilities()
 
-    print('[red] Get Capabilities')
-    print(wps.getcapabilities(xml=None))
-    print('[red] Describe Process')
-    print(wps.describeprocess('vec:VectorZonalStatistics', xml=None))
-    print('[red] Execute Process')
-    my_inputs = [
-        ('zones', 'http://geoserver/wfs', 'text/xml'), ('classification', 'image/tiff', 'image/tiff')]
-    print(wps.execute('vec:VectorZonalStatistics', my_inputs, output='statistics'))
+    print('[red] GET PROCESSES')
+    print(wps.get_processes())
+
+    print('[red] DESCRIBE PROCESS')
+    zonal_stats = wps.describe_wps_process('ras:RasterZonalStatistics')
+    print(zonal_stats)
+    inputs = [x.identifier for x in zonal_stats.dataInputs]
+    print(inputs)
